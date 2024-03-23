1773fe2dfef8cd80a90576ecb9e0a46c9724bfec
Nathan <nathanluyg@gmail.com>
2024-03-23 14:58:46 -0700

    WIP new UI library

--- js/node/workflows.ui.js
+++ js/node/workflows.ui.js
@@ -4,6 +4,12 @@ import { infoDialog } from '../comfy/ui.js';
 import { formatTimestamp, formatDuration } from '../utils.js';
 import { nimbus } from '../resource/index.js';
 
+import { WorkflowRunsTable } from '../ui/table.js';
+import { RunDetails } from '../ui/runDetails.js';
+import van from '../lib/van.js';
+
+const activeTab = van.state(0)
+const runId = van.state(null)
 
 class WorkflowTableDialog extends ComfyDialog {
   container = null;
@@ -44,11 +50,23 @@ class WorkflowTableDialog extends ComfyDialog {
   }
 
   close() {
+    this.container.removeChild(this.component)
+    this.component.remove()
+    //this.innerContainer.remove()
     this.element.style.display = "none";
   }
 
   async show() {
-    await this.showWorkflowsTable();
+    this.component = document.createElement("div");
+    van.add(this.component, () => van.tags.div(
+      activeTab.val == 0 ? WorkflowRunsTable(activeTab, runId) : RunDetails(activeTab, runId)
+    ))
+
+    this.container.append(this.component)
+
+    this.element.style.display = "flex";
+    this.element.style.zIndex = 1001;
+    //await this.showWorkflowsTable();
   }
 
   async showWorkflowDetails(id) {
