d85de1170962605b891f1ab53fc57bb018b527f7
vindennt <truong.dennis00@gmail.com>
2024-06-11 17:28:24 -0700

    pet appears and animates with comfypets reference code

---
+++ js/assistant/pet.js
@@ -0,0 +1,287 @@
+//import { GIF } from "../libs/gif.js";
+import { GameObject } from './core.js'
+
+// Our sprite sheet is on a grid of 64pxs
+// Each row is 64px tall, and each frame is 64px wide
+const SPRITE_SIZE = 128
+const SPRITE_SHEET = {
+  JUMP: {
+    row: 0,
+    frames: 11,
+  },
+  IDLE1: {
+    row: 1,
+    frames: 5,
+  },
+  IDLE2: {
+    row: 2,
+    frames: 5,
+  },
+  SIT: {
+    row: 3,
+    frames: 9,
+  },
+  WALK: {
+    row: 4,
+    frames: 5,
+  },
+  RUN: {
+    row: 5,
+    frames: 8,
+  },
+  SNIFF: {
+    row: 6,
+    frames: 8,
+  },
+  SNIFF_WALK: {
+    row: 7,
+    frames: 8,
+  },
+}
+
+/**
+ * Base pet class
+ */
+export class Pet extends GameObject {
+  constructor({ x, y, height, width }) {
+    super(x, y, height, width)
+    // Pet state
+    this.x = x
+    this.currentDirection = 'right'
+
+    this.height = height
+    this.width = width
+
+    this.emote = false
+    this.talk = false
+    this.talkText = ''
+
+    this.hungerPoints = 0
+
+    // Properties here tell when the
+    // pet to change directions. Right now
+    // the pet will randomly change directions
+    // after t seconds.
+    this.time = 0
+    this.directionDuration = 0
+
+    // Assets
+    this.petImage = new Image()
+    this.petImage.src =
+      'https://comfyui-output.nyc3.cdn.digitaloceanspaces.com/babycorgi-sprite-128x128.png'
+
+    this.age = 0
+    this._initializePet()
+  }
+
+  async _initializePet() {
+    // Set pet age
+    this.age = 5
+
+    // Set hunger level
+    this.hungerPoints = 5
+  }
+
+  /**
+   * Creates a list of animations from a spritesheet
+   * - e.g. renderWalk, renderSniff_walk, renderIdle1
+   */
+  createSpriteAnimations(image) {
+    Object.keys(SPRITE_SHEET).forEach((animName) => {
+      // transform name to title case
+      // FUNC1 -> Func1
+      const titleCase =
+        animName.charAt(0).toUpperCase() + animName.slice(1).toLowerCase()
+      const funcName = `render${titleCase}`
+
+      const spriteFrames = SPRITE_SHEET[animName].frames
+      const spriteFramesY = SPRITE_SHEET[animName].row
+      this[funcName] = (ctx, renderCount, slowFpsBy = 10) => {
+        this.renderSpriteAnimation(
+          ctx,
+          image,
+          {
+            renderCount,
+            spriteFrames: spriteFrames - 1,
+            spriteFramesY,
+            slowFpsBy,
+          },
+        )
+      }
+    })
+  }
+
+
+  // _chooseRandomDirection() {
+  //   const directions = ['left', 'right', 'idle1', 'idle2']
+
+  //   const changeDirections = () => {
+  //     const randomIndex = Math.floor(Math.random() * directions.length)
+  //     this.currentDirection = directions[randomIndex]
+  //   }
+
+  //   if (Date.now() - this.time > this.directionDuration) {
+  //     changeDirections()
+  //     this.time = Date.now()
+  //     this.directionDuration = Math.random() * 4000 + 1000
+  //   }
+  // }
+
+  // // debug function
+  _showHitBox(ctx) {
+    console.log("showing hitbox pet side)")
+
+    if (!ctx) {
+      console.error("Canvas context (ctx) is undefined.");
+      return;
+    }
+
+    ctx.fillStyle = 'blue'
+    // ctx.fillRect(
+    //   this.x, // x
+    //   this.y,
+    //   this.width,
+    //   this.height,
+    // )
+  }
+
+  // renderOnTop(ctx) {
+  //   ctx.fillStyle = 'blue'
+  //   ctx.fillRect(
+  //     this.x, // x
+  //     this.y - this.height,
+  //     this.width,
+  //     this.height,
+  //   )
+  // }
+
+  // setEmote() {
+  //   // set an emote for t seconds
+  //   this.emote = true
+
+  //   setTimeout(() => {
+  //     this.emote = false
+  //   }, 1000)
+  // }
+
+  // setTalk(text, duration = 1000) {
+  //   // set an emote for t seconds
+  //   this.talk = true
+  //   this.talkText = text
+
+  //   setTimeout(() => {
+  //     this.talk = false
+  //   }, duration)
+  // }
+
+  // onClick() {
+  //   this.setTalk('Woof!')
+  // }
+
+  renderSpriteAnimation(ctx, spriteSheet, frameSettings) {
+    const {
+      renderCount,
+      spriteFrames,
+      spriteFramesY,
+    } = frameSettings
+
+    const _spriteFramesY = SPRITE_SIZE * spriteFramesY
+    const spriteRenderSize = SPRITE_SIZE // This is the final size users see the sprite as
+    // ctx.imageSmoothingEnabled = true
+    // ctx.imageSmoothingQuality = 'high'
+
+    // There is 5 frames in the sprite sheet for walking
+    // so instead of doing this.renderCount % 4 (0 - 5 frames),
+    // we do 0 - 50 frames and scale down for a lower image fps.
+    const _frame = renderCount % (spriteFrames )
+    const frame = Math.round(_frame)
+
+    const currentRenderFrame = SPRITE_SIZE * frame
+
+    // Offset
+    const offsetX = (spriteRenderSize - this.width) / 2
+    const offsetY = (spriteRenderSize - this.height) / 2
+
+    if (ctx?.drawImage) {
+      ctx.drawImage(
+        spriteSheet,
+        currentRenderFrame,
+        _spriteFramesY,
+        SPRITE_SIZE,
+        SPRITE_SIZE,
+        this.x - offsetX,
+        this.y - offsetY,
+        spriteRenderSize,
+        spriteRenderSize,
+      )
+    }
+    
+  }
+
+
+  move(ctx, renderCount) {
+    // switch (this.currentDirection) {
+    //   default:
+    //   }
+    this.renderIdle2(ctx, renderCount)
+  }
+
+
+  // renderTextBubble(ctx) {
+  //   ctx.fillStyle = 'black'
+  //   ctx.font = '14px Courier New'
+  //   ctx.textAlign = 'center'
+  //   ctx.textBaseline = 'middle'
+
+  //   const textBubbleWidth = 70
+  //   const textBubbleHeight = 40
+  //   if (this.currentDirection == 'left') {
+  //     const textBubbleX = -this.x - this.width + textBubbleWidth
+  //     const textBubbleY = this.y - textBubbleHeight
+  //     ctx.save()
+  //     ctx.scale(-1, 1)
+  //     ctx.drawImage(
+  //       this.textBubble,
+  //       textBubbleX,
+  //       textBubbleY,
+  //       textBubbleWidth,
+  //       textBubbleHeight,
+  //     )
+  //     ctx.restore()
+
+  //     // Calculate text position
+  //     const textX = this.x - textBubbleWidth / 2
+  //     const textY = textBubbleY + textBubbleHeight / 2
+
+  //     ctx.fillText(this.talkText, textX, textY)
+  //   } else {
+  //     // this includes idle, and everything
+  //     // else at the moment
+  //     const textBubbleX = this.x + this.width
+  //     const textBubbleY = this.y - textBubbleHeight
+
+  //     ctx.drawImage(
+  //       this.textBubble,
+  //       textBubbleX,
+  //       textBubbleY,
+  //       textBubbleWidth,
+  //       textBubbleHeight,
+  //     )
+
+  //     const textX = textBubbleX + textBubbleWidth / 2
+  //     const textY = textBubbleY + textBubbleHeight / 2
+
+  //     ctx.fillText(this.talkText, textX, textY)
+  //   }
+  // }
+
+  render(ctx, renderCount) {
+    console.log("render (pet side)")
+
+    this.createSpriteAnimations(this.petImage)
+
+    // move the pet
+    this._showHitBox(ctx)
+    this.move(ctx, renderCount)
+  }
+}
--- js/button/actions.js
+++ js/button/actions.js
@@ -1,129 +1,134 @@
-import { 
-  resolveDependencies,
-  pollSyncDependencies, 
-} from "./dependencies.js"
+// import { 
+//   resolveDependencies,
+//   pollSyncDependencies, 
+// } from "./dependencies.js"
 import { 
   createMetaNode, 
-  getWorkflowName,
-  setWorkflowId,
-  getWorkflowId,
-  getApiToken, 
-  validatePrompt, 
-  compareWorkflows, 
-  isWorkflowUpToDate
+  // getWorkflowName,
+  // setWorkflowId,
+  // getWorkflowId,
+  // getApiToken, 
+  // validatePrompt, 
+  // compareWorkflows, 
+  // isWorkflowUpToDate
 } from "../utils.js"
-import { app } from '../comfy/comfy.js'; 
+// import { app } from '../comfy/comfy.js'; 
 import { infoDialog } from '../comfy/ui.js'; 
 import { 
   setButtonDefault,
-  setButtonLoading,
+  // setButtonLoading,
   setMessage,
 } from './ui.js'; 
-import { authDialog } from '../auth/index.js';
-import { nimbus, local } from '../resource/index.js';
-import { endpoint } from '../constants.js';
+// import { authDialog } from '../auth/index.js';
+// import { nimbus, local } from '../resource/index.js';
+// import { endpoint } from '../constants.js';
 
-import { ComfyCloudDialog, ComfyCloudPopover } from '../comfy/ui.js';
+import { 
+  // ComfyCloudDialog,
+  ComfyCloudPopover } from '../comfy/ui.js';
 
-import { taskId, Progress } from '../ui/uploadProgress.js';
+import {
+  //  taskId,
+   Progress } from '../ui/uploadProgress.js';
 
 export const progressDialog = new ComfyCloudPopover(Progress, "Uploading dependencies...")
 
 export async function onGeneration() {
   try {
-    setButtonDefault()
+    await createMetaNode();
+  //   setButtonDefault()
 
-    // check auth
-    const apiToken = getApiToken();
-    const doesApiTokenExist = !!apiToken;
+  //   // check auth
+  //   const apiToken = getApiToken();
+  //   const doesApiTokenExist = !!apiToken;
 
-    if(!doesApiTokenExist) {
-      // Request auth
-      setButtonDefault()
-      return authDialog.show();
-    }
+  //   if(!doesApiTokenExist) {
+  //     // Request auth
+  //     setButtonDefault()
+  //     return authDialog.show();
+  //   }
 
-    // check if ComfyCloud meta node exists
-    const deployMeta = app.graph.findNodesByType("ComfyCloud");
-    let isNewWorkflow = deployMeta.length == 0
+  //   // check if ComfyCloud meta node exists
+  //   const deployMeta = app.graph.findNodesByType("ComfyCloud");
+  //   let isNewWorkflow = deployMeta.length == 0
 
-    // This case usually happens when user manually adds the ComfyCloud node
-    // and doesn't delete it
-    const hasNoId = !isNewWorkflow && !getWorkflowId()
-    if(hasNoId) {
-      app.graph.remove(deployMeta[0])
-      isNewWorkflow = true
-    }
+  //   // This case usually happens when user manually adds the ComfyCloud node
+  //   // and doesn't delete it
+  //   const hasNoId = !isNewWorkflow && !getWorkflowId()
+  //   if(hasNoId) {
+  //     app.graph.remove(deployMeta[0])
+  //     isNewWorkflow = true
+  //   }
 
-    const localWorkflow = await app.graphToPrompt();
-    const isValid = await validatePrompt(localWorkflow.output);
-    if(!isValid) {
-      throw new Error("Prompt is not valid")
-    }
+  //   const localWorkflow = await app.graphToPrompt();
+  //   const isValid = await validatePrompt(localWorkflow.output);
+  //   if(!isValid) {
+  //     throw new Error("Prompt is not valid")
+  //   }
 
-    // Start execution
-    setButtonLoading();
+  //   // Start execution
+  //   setButtonLoading();
 
-    if(isNewWorkflow) {
-      // Wait for user to input workflow name
-      await createMetaNode();
-      //await createEmptyWorkflow()
-      const newWorkflow = await nimbus.workflow.create({ 
-        name: getWorkflowName(),
-      })
-      setWorkflowId(newWorkflow.id)
-
-
-      setMessage("Creating new workflow. This may take awhile");
-    }
-
-    // compare workflow
-    const existing_workflow = await nimbus.workflow.retrieve()
-
-    const diffDeps = compareWorkflows(localWorkflow.output, existing_workflow.workflow_api);
-
-    // sync workflow
-    if(!isWorkflowUpToDate(diffDeps)) {
-      setMessage("Syncing dependencies...");
-
-      const { dependencies, workflow_patch } = await resolveDependencies(diffDeps)
-      const res = await local.uploadDependencies({
-        workflow_id: getWorkflowId(),
-        endpoint,
-        ...dependencies,
-      })
+  //   if(isNewWorkflow) {
+  //     // Wait for user to input workflow name
+  //     await createMetaNode();
+  //     //await createEmptyWorkflow()
+  //     const newWorkflow = await nimbus.workflow.create({ 
+  //       name: getWorkflowName(),
+  //     })
+  //     setWorkflowId(newWorkflow.id)
 
 
-      const uploadTaskId = res.task_id
-      if(uploadTaskId) {
-        taskId.val = uploadTaskId
-        // Open UI window
-        progressDialog.show();
+  //     setMessage("Creating new workflow. This may take awhile");
+  //   }
+
+  //   // compare workflow
+  //   const existing_workflow = await nimbus.workflow.retrieve()
+
+  //   const diffDeps = compareWorkflows(localWorkflow.output, existing_workflow.workflow_api);
+
+  //   // sync workflow
+  //   if(!isWorkflowUpToDate(diffDeps)) {
+  //     setMessage("Syncing dependencies...");
+
+  //     const { dependencies, workflow_patch } = await resolveDependencies(diffDeps)
+  //     const res = await local.uploadDependencies({
+  //       workflow_id: getWorkflowId(),
+  //       endpoint,
+  //       ...dependencies,
+  //     })
+
+
+  //     const uploadTaskId = res.task_id
+  //     if(uploadTaskId) {
+  //       taskId.val = uploadTaskId
+  //       // Open UI window
+  //       progressDialog.show();
       
-        await pollSyncDependencies(uploadTaskId)
-      }
+  //       await pollSyncDependencies(uploadTaskId)
+  //     }
 
-      setMessage("Updating workflow...");
+  //     setMessage("Updating workflow...");
 
-      await nimbus.workflow.update({
-        workflow: localWorkflow.workflow,
-        workflow_api: localWorkflow.output,
-        workflow_patch: workflow_patch,
-        dependencies: dependencies,
-      })
-    }
+  //     await nimbus.workflow.update({
+  //       workflow: localWorkflow.workflow,
+  //       workflow_api: localWorkflow.output,
+  //       workflow_patch: workflow_patch,
+  //       dependencies: dependencies,
+  //     })
+  //   }
 
-    // Beyond this point, we assume all dependencies
-    // and workflow api is synced to the cloud
+  //   // Beyond this point, we assume all dependencies
+  //   // and workflow api is synced to the cloud
     
-    // create run
-    //await createRun()
-    await nimbus.workflowRun.create()
+  //   // create run
+  //   //await createRun()
+  //   await nimbus.workflowRun.create()
 
-    infoDialog.showMessage(
-      "Item queued!",
-      "You can view your generation results by clicking the 'Menu' button in your Comfy Cloud custom node."
-    )
+  //   infoDialog.showMessage(
+  //     "Item queued!",
+  //     "You can view your generation results by clicking the 'Menu' button in your Comfy Cloud custom node."
+  //   )
   } catch (e) {
     // handle error
     infoDialog.showMessage("Error", e);
