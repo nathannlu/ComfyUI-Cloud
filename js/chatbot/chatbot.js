12a953fa845d6e3974dde1c7317ee049f53e0f89
vindennt <truong.dennis00@gmail.com>
2024-07-19 20:53:06 -0700

    refactor: conforms chatbot to nombis endpoint builder

--- js/chatbot/chatbot.js
+++ js/chatbot/chatbot.js
@@ -1,31 +1,16 @@
-// import { endpoint } from "../constants";
-const endpoint = "http://localhost:4000/"; // Switch to live  as needed
+import { nimbus } from "../resource/index.js";
 
-export async function getBotResponse(message, token) {
-  console.log("calling the gpt bot woith message", message);
-  try {
-    const response = await fetch(`${endpoint}chat/send-message`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ message: message, origin: "ComfyUI Chat" }),
-    });
+export async function getBotResponse(message) {
+    try {
+        const data = await nimbus.chatbot.sendMessage({
+            message: message,
+            origin: "ComfyUI Cloud Chat"
+        })
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
+        return data
+    } catch (e) {
+        throw new Error(e.message)
     }
-
-    console.log("response", response);
-
-    const responseData = await response.json();
-
-    return responseData;
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-    throw error;
-  }
 }
 
 export function isValidWorkflow(workflow) {
