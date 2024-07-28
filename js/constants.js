c10f43fc1581bd1bf092088bd79151b22c032352
Nathan <nathanluyg@gmail.com>
2024-07-27 17:15:34 -0700

    Updated server urls

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,4 @@
 //export const endpoint = "https://comfycloud.vercel.app"
 //export const endpoint = "http://localhost:4000"
-export const endpoint = "https://comfyui-cloud-a0cf78bd8c3d.herokuapp.com"
+//export const endpoint = "https://comfyui-cloud-a0cf78bd8c3d.herokuapp.com"
+export const endpoint = "http://159.223.105.118/api"
